34283ad496b0c07547ae3f0b25d8c80bc334865c
gkrafty <18251717+gkrafty@users.noreply.github.com>
2021-12-22 17:19:32 -0600

    Adding Random Message Display

--- .motd/motd.sh
+++ .motd/motd.sh
@@ -8,3 +8,12 @@ printf "Welcome, ${user}. It's currently ${date} "
 #echo "System:"
 #system_profiler SPSoftwareDataType | tail -n10 | cut -c 5- | lolcat
 echo
+
+##########################
+#### Random MOTD
+##########################
+#echo
+#export MOTD_CMD=(sed -n $((1+$RANDOM%`wc -l ~/.motd/motd-msg | cut -f 7 -d ' '`))p ~/.motd/motd-msg)
+#echo "$($MOTD_CMD)"
+sed -n $((1+$RANDOM%`wc -l ~/.motd/motd-msg | cut -f 7 -d ' '`))p ~/.motd/motd-msg
+#echo
